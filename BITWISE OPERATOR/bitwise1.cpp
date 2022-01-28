55e010f8343bceebd9864fea5d01d544dad3489f
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-28 11:30:25 +0530

    initial commit

---
+++ BITWISE OPERATOR/bitwise1.cpp
@@ -0,0 +1,15 @@
+// c++ program to demonstarte about bitwise operator.
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a = 4;
+  int b = 5;
+
+  std::cout <<"a&b: "<< (a&b) << '\n';
+  std::cout <<"a|b: "<< (a|b) << '\n';
+  std::cout <<"~a: "<< ~a << '\n';
+  std::cout << "a^b" <<(a^b)<<'\n';
+
+
+}

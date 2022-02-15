f1ee3ff77d3f2c3e58155885c9717de8d4c799a9
deepak2810 <deepakkhanka.97@gmail.com>
2022-02-15 23:36:46 +0530

    string input using getline method

---
+++ WIPRO/String/str2.cpp
@@ -0,0 +1,15 @@
+// Simple string program to take input string with spaces.
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string str;
+  getline(cin,str);
+
+
+
+  cout<<str<<str.length();
+
+  return 0;
+}

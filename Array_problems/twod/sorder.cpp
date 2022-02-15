8a97a65b6e6fd5ef2d2d059daf6836b0a7f41a20
deepak2810 <deepakkhanka.97@gmail.com>
2022-02-15 23:29:15 +0530

    strings

---
+++ Array_problems/twod/sorder.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int m,n;
+  std::cin >> m>> n;
+
+  int arr[m][n];
+
+  for( int i =0; i<n; i++){
+    for( int j = 0; j<m; j++){
+      std::cin >> arr[i][j];
+    }
+  }
+
+  int
+
+
+}
---
+++ WIPRO/String/st1.cpp
@@ -0,0 +1,22 @@
+ // a simple program to declare a string.
+
+#include<iostream>
+// Header file to use string data type.
+#include<string>
+using namespace std;
+
+// function to count the number of character in strings.
+int Count(string s){
+  int size = s.size();
+  return size;
+}
+int main(){
+
+  string s1 ;
+  std::cout << "Please enter something : " << '\n';
+  std::cin >> s1;
+
+  std::cout << "length of string is : "<<Count(s1) << '\n';
+
+
+}

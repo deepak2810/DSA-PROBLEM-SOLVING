6321589a00c9a520cab7a2d70492a6160d4481aa
deepak2810 <deepakkhanka.97@gmail.com>
2022-03-09 00:20:03 +0530

    character pointer

---
+++ LESGO/charpoi2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  char a  = 'abc';
+
+  char *p = &a;
+
+  std::cout << a << '\n';
+  std::cout << p << '\n';
+
+
+}
---
+++ LESGO/charpointer3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  char c1 = 'd';
+  char *pc = &c1;
+
+  // Value of c1
+  std::cout << c1 << '\n';
+
+  // printing add .
+  // start printing from a untill it finds zero.
+ std::cout << pc << '\n';
+}

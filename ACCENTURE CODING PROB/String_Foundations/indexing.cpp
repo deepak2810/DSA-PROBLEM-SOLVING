79fc54de0a2c58c6f06a1153ce21d2126b6c0eb7
deepak2810 <deepakkhanka.97@gmail.com>
2022-04-19 21:09:11 +0530

    new test

--- ACCENTURE CODING PROB/String_Foundations/indexing.cpp
+++ ACCENTURE CODING PROB/String_Foundations/indexing.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-  string str;
+  string str,str1;
   std::cin >> str;
+  std::cin >> str1;
+
+  if(str==str1){
+    std::cout << " strings are Equal" << '\n';
+  }
+  else{
+    std::cout << "Not equal" << '\n';
+  }
 
   std::cout << str[0] << '\n';
 }

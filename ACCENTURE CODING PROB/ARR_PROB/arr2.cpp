c74766e1ab27f1312ba19667583db983be6a455c
deepak2810 <deepakkhanka.97@gmail.com>
2022-04-29 10:58:03 +0530

    seg array

---
+++ ACCENTURE CODING PROB/ARR_PROB/arr2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+void segregate(int arr[], int n){
+
+  int s = 0;
+  int e = n-1;
+
+  while(s<e){
+    for(int i = 0; i<n; i++){
+
+      if(arr[s]%2 == 0){
+        s++;
+      }
+
+      else{
+        swap(arr[s],arr[e]);
+        e--;
+      }
+
+    }
+
+    return ;
+  }
+
+
+
+
+}
+
+
+int main(){
+  int n;
+  std::cin >> n;
+
+  int arr[n];
+
+  for(int i = 0; i<n; i++){
+    std::cin >> arr[i];
+  }
+
+  segregate(arr,n);
+
+  for(int i = 0; i<n; i++){
+    std::cout << arr[i] <<" ";
+  }
+
+
+
+}

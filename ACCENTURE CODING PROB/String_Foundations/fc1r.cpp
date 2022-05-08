a336e33f23f83d13fb8b9d0e489729654a7341b2
deepak2810 <deepakkhanka.97@gmail.com>
2022-05-08 19:06:32 +0530

    lets get started again ..

---
+++ ACCENTURE CODING PROB/String_Foundations/fc1r.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int factorial(int n ){
+  int fact = 1;
+
+  for(int i = 1; i<=n; i++){
+    fact *= i;
+  }
+
+  return fact;
+
+}
+int main(){
+  int a;
+  std::cin >> a;
+
+  std::cout << factorial(a) << '\n';
+
+  return 0;
+}

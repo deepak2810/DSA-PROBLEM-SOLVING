ad8f31f4793aa4df928f8f56e018f18e53ff2f18
deepak2810 <deepakkhanka.97@gmail.com>
2022-06-02 16:16:32 +0530

    ..

---
+++ ACCENTURE CODING PROB/onTheSpot/Day07/subsum.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  std::cin >> n;
+
+  int arr[n];
+
+  for(int i=0; i<n; i++){
+    std::cin >> arr[i];
+  }
+
+  int check;
+  std::cin >> check;
+
+  int flag, start,sum = 0;
+
+
+  for(int i = 0; i<n; i++ ){
+   sum+=arr[i];
+
+  while(sum>check){
+    sum=sum-arr[start];
+    start++;
+  }
+
+  if(sum==check){
+    std::cout << start+1<<" "<<i+1 << '\n';
+    flag = 1;
+    break;
+  }
+  }
+
+if (flag==0) {
+    /* code */
+    std::cout << "-1" << '\n';
+}
+ return 0;
+
+}
